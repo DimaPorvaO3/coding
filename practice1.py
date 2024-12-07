c1ceb5b450c1ea61b2b14c08068b1862600163eb
DimaPorvaO3 <dimaporva2007@gmail.com>
2024-12-07 18:53:07 +0200

    Add files via upload

---
+++ practice1.py
@@ -0,0 +1,8 @@
+a = "Hello World"
+name = "Dima"
+surname = "Porva"
+age = 17
+print(f"значення та тип  a: {type(a)}, {a}")
+print(f"значення та тип name: {type(name)}, {name}")
+print(f"значення та тип surname: {type(surname)}, {surname}")
+print(f"значення та тип age: {type(age)}, {age}")
